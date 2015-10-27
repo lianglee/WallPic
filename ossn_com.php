5c8e0d84fd00956b796fe8c47cad53ba416ff4be
lianglee <manactric@gmail.com>
2015-10-27 23:14:46 +0500

    initial release

---
+++ ossn_com.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+
+function reposition_wall_image(){
+	
+}
+ossn_register_callback('ossn', 'init', 'reposition_wall_image');
